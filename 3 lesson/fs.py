4c207244266432d9862f604db654e07d7069afed
erikdark <>
2023-02-13 20:15:47 +0300

    3 lesson

---
+++ 3 lesson/fs.py
@@ -0,0 +1,16 @@
+a = int(input())
+if a <= 13:
+    print('net')
+else:
+    if a >= 13:
+        print('yes')
+        b = int(input())
+        if b == 10:
+            print("GOTOVO")
+        else:
+            print('NA VYHOD')
+
+
+X     Y    NOT X   X OR Y  AND Y
+
+F     F      T       F      F
